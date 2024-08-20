3d15039fe51220655dbe073664408c07d55b4e89
João Lucas Felix de Macena <131909907+Joao-lucas-felix@users.noreply.github.com>
2024-08-20 18:55:25 -0300

    Feat: spring project bootstrap

---
+++ API/ProjetoBancoDeDados/src/main/java/com/joao_lucas_felix/ProjetoBancoDeDados/ProjetoBancoDeDadosApplication.java
@@ -0,0 +1,13 @@
+package com.joao_lucas_felix.ProjetoBancoDeDados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBancoDeDadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBancoDeDadosApplication.class, args);
+	}
+
+}
---
+++ API/ProjetoBancoDeDados/src/test/java/com/joao_lucas_felix/ProjetoBancoDeDados/ProjetoBancoDeDadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joao_lucas_felix.ProjetoBancoDeDados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBancoDeDadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
