f61d9fc258afd0dc809e86c166498105bf8f3064
João Lucas Felix de Macena <131909907+Joao-lucas-felix@users.noreply.github.com>
2024-08-21 18:49:37 -0300

    refactor: change the order table to save the quantity of books in the order

--- API/ProjetoBancoDeDados/src/main/resources/db/migration/V5__CREATE_TABLE_PEDIDOS.sql
+++ API/ProjetoBancoDeDados/src/main/resources/db/migration/V5__CREATE_TABLE_PEDIDOS.sql
@@ -2,7 +2,9 @@ CREATE  TABLE IF NOT EXISTS pedido(
     id_usuario BIGINT NOT NULL,
     id_livro BIGINT NOT NULL,
     data DATE NOT NULL,
+    quantidade INT NOT NULL DEFAULT 1,
     PRIMARY KEY(id_livro, id_usuario, data),
     CONSTRAINT `fk_usurario` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`),
-    CONSTRAINT `fk_livro` FOREIGN KEY (`id_livro`) REFERENCES `livro` (`id`)
+    CONSTRAINT `fk_livro` FOREIGN KEY (`id_livro`) REFERENCES `livro` (`id`),
+    CONSTRAINT `quantidade_maior_0` CHECK (quantidade > 0 )
 )
