b99683b63f3a81845e3820c9411c30602e3c6cba
João Lucas Felix de Macena <131909907+Joao-lucas-felix@users.noreply.github.com>
2024-08-22 18:49:40 -0300

    Refactor: change the Order class, to it has the correct key

--- API/ProjetoBancoDeDados/src/main/java/com/joao_lucas_felix/ProjetoBancoDeDados/domain/Pedido.java
+++ API/ProjetoBancoDeDados/src/main/java/com/joao_lucas_felix/ProjetoBancoDeDados/domain/Pedido.java
@@ -1,11 +1,14 @@
 package com.joao_lucas_felix.ProjetoBancoDeDados.domain;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import lombok.*;
-
-import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "pedido")
@@ -15,13 +18,13 @@ import java.util.Date;
 @AllArgsConstructor
 @EqualsAndHashCode
 public class Pedido {
-    @Column(name = "id_user")
-    private Long idUser;
-    @Column(name = "id_livro")
-    private Long idLivro;
-    @Column(name = "data")
-    private Date data;
+
+    @EmbeddedId
+    private PedidoId id;
+
     @Column(name = "quantidade")
     private Integer quantidade;
-
 }
+
+
+
---
+++ API/ProjetoBancoDeDados/src/main/java/com/joao_lucas_felix/ProjetoBancoDeDados/domain/PedidoId.java
@@ -0,0 +1,25 @@
+package com.joao_lucas_felix.ProjetoBancoDeDados.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Embeddable
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class PedidoId implements Serializable {
+    @Column(name = "id_user")
+    private Long idUser;
+
+    @Column(name = "id_livro")
+    private Long idLivro;
+
+    @Column(name = "data")
+    private Date data;
+}
