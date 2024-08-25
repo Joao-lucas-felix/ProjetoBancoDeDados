7f65753b6e1648eb43a169235d06e729dddc090b
João Lucas Felix de Macena <131909907+Joao-lucas-felix@users.noreply.github.com>
2024-08-25 11:52:33 -0300

    Refactor: change the pedido table

--- API/ProjetoBancoDeDados/src/main/resources/db/migration/V5__CREATE_TABLE_PEDIDOS.sql
+++ API/ProjetoBancoDeDados/src/main/resources/db/migration/V5__CREATE_TABLE_PEDIDOS.sql
@@ -1,9 +1,10 @@
 CREATE  TABLE IF NOT EXISTS pedido(
+    id BIGINT NOT NULL AUTO_INCREMENT,
     id_usuario BIGINT NOT NULL,
     id_livro BIGINT NOT NULL,
     data DATE NOT NULL,
     quantidade INT NOT NULL DEFAULT 1,
-    PRIMARY KEY(id_livro, id_usuario, data),
+    PRIMARY KEY (id),
     CONSTRAINT `fk_usurario` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`),
     CONSTRAINT `fk_livro` FOREIGN KEY (`id_livro`) REFERENCES `livro` (`id`),
     CONSTRAINT `quantidade_maior_0` CHECK (quantidade > 0 )
