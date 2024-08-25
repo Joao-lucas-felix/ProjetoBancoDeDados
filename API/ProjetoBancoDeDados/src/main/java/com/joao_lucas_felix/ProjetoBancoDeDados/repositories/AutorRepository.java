a73ac457d4941c96fb9d786be7b8be7bda651ed2
João Lucas Felix de Macena <131909907+Joao-lucas-felix@users.noreply.github.com>
2024-08-25 11:50:12 -0300

    Feat: Create the method findAutorByName

--- API/ProjetoBancoDeDados/src/main/java/com/joao_lucas_felix/ProjetoBancoDeDados/repositories/AutorRepository.java
+++ API/ProjetoBancoDeDados/src/main/java/com/joao_lucas_felix/ProjetoBancoDeDados/repositories/AutorRepository.java
@@ -1,9 +1,15 @@
 package com.joao_lucas_felix.ProjetoBancoDeDados.repositories;
 
 import com.joao_lucas_felix.ProjetoBancoDeDados.domain.Autor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AutorRepository extends JpaRepository<Autor, Long> {
+    @Query("SELECT a FROM Autor a WHERE a.nome LIKE LOWER(CONCAT ('%',:nome,'%'))")
+    Page<Autor> findAutorByName(@Param("nome") String nome, Pageable pageable);
 }
